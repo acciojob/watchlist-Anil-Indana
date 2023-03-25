cdfb6e145e34ea27ea33f0dee242c336a1b9ec27
unknown <you@example.com>
2023-03-25 16:18:08 +0530

    Try-1

---
+++ src/main/java/com/driver/MovieRepository.java
@@ -0,0 +1,80 @@
+package com.driver;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+public class MovieRepository {
+    private HashMap<String,List<String>> movieDirectorHashMap = new HashMap<>();
+    private HashMap<String,Director> directorHashMap = new HashMap<>();
+    private HashMap<String,Movie> movieHashMap = new HashMap<>();
+    public String addMovie(Movie movie){
+        String key = movie.getName();
+        movieHashMap.put(key,movie);
+
+        return "Movie Added";
+    }
+    public String  addDirector(Director director){
+        String key = director.getName();
+        directorHashMap.put(key,director);
+
+        return "Director Added";
+    }
+    public String   addMovieDirectorPair(String movieName,String directorName){
+        List<String> list;
+        if(movieHashMap.containsKey(directorName)){
+            list = movieDirectorHashMap.get(directorName);
+        }
+        else{
+            list = new ArrayList<>();
+        }
+        list.add(movieName);
+        movieDirectorHashMap.put(directorName,list);
+        return "Movie and Director Paired";
+    }
+    public String deleteDirectorByName(String directorName){
+        directorHashMap.remove(directorName);
+        List<String> movies = movieDirectorHashMap.get(directorName);
+        movieDirectorHashMap.remove(directorName);
+        for(String movieName : movies){
+            movieHashMap.remove(movieName);
+        }
+        return "Deleted by Director name";
+    }
+    public String deleteAllDirectors(){
+        for(String directorName : directorHashMap.keySet()){
+            directorHashMap.remove(directorName);
+            List<String> movies = movieDirectorHashMap.get(directorName);
+            movieDirectorHashMap.remove(directorName);
+            for(String movieName : movies){
+                movieHashMap.remove(movieName);
+            }
+        }
+        return "Deleted";
+    }
+    public List<String> getMoviesByDirectorName(String directorName){
+        return movieDirectorHashMap.get(directorName);
+    }
+    public List<String> findAllMovies(){
+        List<String> movies = new ArrayList<>();
+        for(String movie : movieHashMap.keySet()){
+            movies.add(movie);
+        }
+        return movies;
+    }
+
+    public HashMap<String, Director> getDirectorHashMap() {
+        return directorHashMap;
+    }
+
+    public HashMap<String, Movie> getMovieHashMap() {
+        return movieHashMap;
+    }
+}
+
---
+++ src/main/java/com/driver/MovieService.java
@@ -0,0 +1,42 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.security.DigestException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MovieService {
+    @Autowired
+    MovieRepository movieRepository;
+    public String addMovie(Movie movie){
+        return movieRepository.addMovie(movie);
+    }
+    public String addDirector(Director director){
+       return movieRepository.addDirector(director);
+    }
+    public String addMovieDirectorPair(String movieName,String directorName){
+        return movieRepository.addMovieDirectorPair(movieName,directorName);
+    }
+    public Movie getMovieByName(String name){
+        return  movieRepository.getMovieHashMap().get(name);
+    }
+    public Director getDirectorByName(String name){
+        return movieRepository.getDirectorHashMap().get(name);
+    }
+    public List<String> getMoviesByDirectorName(String name){
+        return movieRepository.getMoviesByDirectorName(name);
+    }
+    public List<String> findAllMovies(){
+        return movieRepository.findAllMovies();
+    }
+    public String deleteDirectorByName(String directorName){
+        return movieRepository.deleteDirectorByName(directorName);
+    }
+    public String deleteAllDirectors(){
+        return movieRepository.deleteAllDirectors();
+    }
+}
